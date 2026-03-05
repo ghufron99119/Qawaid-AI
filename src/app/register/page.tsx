0c21da884b602b6742d314b5f873ea8dc0b724cb
Nailul <240605110160@student.uin-malang.ac.id>
2026-03-05 10:17:44 +0700

    fix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "next";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { signIn } from "next-auth/react";
